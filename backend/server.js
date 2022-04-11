b1eca11043be5afc6cfbfd0a06b3b4bae2cba534
Tae Hyeon Jo <hillskuti@naver.com>
2022-04-11 15:50:48 +0900

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,6 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running.....");
-});
-
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
@@ -34,9 +30,21 @@ app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
 
-// __dirname : point to the current directory
+// __dirname : 현재 디렉토리 지정
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "product") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running.....");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
