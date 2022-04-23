1cc05dc6aac0c7791883b8527efe6d79806ba368
Tae Hyeon Jo <hillskuti@naver.com>
2022-04-23 22:46:03 +0900

    add to comment HomeScreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -13,14 +13,18 @@ import Meta from "../components/Meta";
 import { listProducts } from "../actions/productActions";
 import "./HomeScreen.css";
 
+// 홈 화면
 const HomeScreen = () => {
+  // URL의 검색 키워드와 페이지 수 파라미터를 가져옴
   const { keyword, pageNumber } = useParams();
   const dispatch = useDispatch();
 
   useEffect(() => {
+    // 파라미터에 있는 검색 키워드와 페이지 수를 전달
     dispatch(listProducts(keyword, pageNumber));
   }, [dispatch, keyword, pageNumber]);
 
+  // 상품 리스트를 Store에서 가져오기
   const productList = useSelector((state) => state.productList);
   const { loading, error, products, page, pages } = productList;
 
@@ -31,6 +35,7 @@ const HomeScreen = () => {
         <Loader />
       ) : !keyword ? (
         <>
+          {/* 이벤트 슬라이더 컴포넌트 */}
           <EventCarousel products={products} />
         </>
       ) : (
@@ -43,6 +48,7 @@ const HomeScreen = () => {
 
       <div className="product-container">
         <h2>뭉크몰에서 추천하는 아이템!</h2>
+        {/* 로딩 값이 true인 경우 로딩바 나타남*/}
         {loading ? (
           <Loader />
         ) : error ? (
@@ -50,12 +56,14 @@ const HomeScreen = () => {
         ) : (
           <>
             <Row>
+              {/* 상품리스트를 뿌려주는 영역 */}
               {products.map((product) => (
                 <Col key={product._id} xs={6} sm={6} md={4} lg={4} xl={3}>
                   <Product product={product} />
                 </Col>
               ))}
             </Row>
+            {/* 상품 페이지 넘버 컴포넌트 */}
             <Paginate
               pages={pages}
               page={page}
@@ -65,6 +73,7 @@ const HomeScreen = () => {
         )}
       </div>
       {!keyword ? (
+        // 인스타그램 사진 영역
         <Instagram />
       ) : (
         <div className="product-container">
--- frontend/src/store/orderDeliverSlice.js
+++ frontend/src/store/orderDeliverSlice.js
@@ -4,17 +4,21 @@ const orderDeliverSlice = createSlice({
   name: "orderDeliver",
   initialState: {},
   reducers: {
+    // 배송 상태 요청
     ORDER_DELIVER_REQUEST(state) {
       state.loading = true;
     },
+    // 배송 상태 성공
     ORDER_DELIVER_SUCCESS(state) {
       state.loading = false;
       state.success = true;
     },
+    // 배송 상태 실패
     ORDER_DELIVER_FAIL(state, action) {
       state.loading = false;
       state.error = action.payload;
     },
+    // 주문 State 초기화
     ORDER_DELIVER_RESET() {
       return {};
     },
